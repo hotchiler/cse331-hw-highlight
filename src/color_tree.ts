d7746d3426371c85abbc07c688cac30aefb914b0
alial <aaldhahi@uw.edu>
2024-04-30 13:21:48 -0700

    3a 3b

--- src/color_tree.ts
+++ src/color_tree.ts
@@ -1,15 +1,26 @@
-// import { List } from './list';
-// import { ColorDetails } from './colors';
-// import { ColorNode, empty } from './color_node';
+import { List, len, split, nil } from './list';
+import { ColorDetails, COLORS } from './colors';
+import { ColorNode, empty, node } from './color_node';
 
-// TODO: Uncomment, implement, and write JSDoc
 
-// export const buildBst = (L: List<ColorDetails>): ColorNode => {
-//    // Note: the implementation of this function requires you to use
-//    //   Math.floor() which takes a number, so you will need to use BigInt()
-//    //    and Number() to convert types as needed
-// };
-
+/**
+ * Builds a binary search tree (BST) from a given list of color details.
+ *
+ * @param {List<ColorDetails>} L - The list of color details.
+ * @returns {ColorNode} The root node of the BST.
+ */
+export const buildBst = (L: List<ColorDetails>): ColorNode => {
+    if (L === nil) {
+        return empty;
+    }
+    const m = Math.floor(Number(len(L)) / 2);
+    const [P, S] = split(BigInt(m), L);
+    if (S !== nil && S.kind === 'cons') {
+        const [b, R] = [S.hd, S.tl];
+        return node(b, buildBst(P), buildBst(R));
+    }
+    return buildBst(P);
+}
 // export const search = (y: string, root: ColorNode): ColorDetails | undefined => {
 //     
 // };
--- src/color_tree_test.ts
+++ src/color_tree_test.ts
@@ -1,14 +1,38 @@
-// import * as assert from 'assert';
+import * as assert from 'assert';
+import { explode_array, nil} from "./list";
+import {buildBst} from "./color_tree";
+import { node, empty } from "./color_node";
 
 describe('color_tree', function() {
 
     // TODO: Uncomment given examples and add more test cases in 3
 
-    it('buildBst', function() {
-        // assert.deepEqual(buildBst(explode_array([
-        //     ['Blue', '#0000FF', true],
-        //   ])), node(['Blue', '#0000FF', true], empty, empty));
-    });
+
+        it('build_bst', function() {
+
+            // Base Case - 0 recursive calls:
+            assert.deepStrictEqual(buildBst(nil), empty);
+
+            // 0-1-Many Heuristic - 1 recursive calls:
+            assert.deepStrictEqual(buildBst(explode_array([
+                ['Blue', '#0000FF', true],
+              ])), node(['Blue', '#0000FF', true], empty, empty));
+
+            assert.deepStrictEqual(buildBst(explode_array([
+                ['Orange', '#FFA500', true],
+            ])), node(['Orange', '#FFA500', true], empty, empty));
+
+            // 0-1-Many Heuristic - Many recursive calls:
+            assert.deepStrictEqual(buildBst(explode_array([
+                ['Brown', '#964B00', true], ['Purple', '#800080', true]
+            ])), node(['Purple', '#800080', true], node(['Brown', '#964B00', true], empty, empty), empty));
+
+            assert.deepStrictEqual(buildBst(explode_array([
+                ['Lime', '#bfff00', false], ['White', '#FFFFFF', false], ['Yellow', '#FFFF00', false],])),
+                node(['White', '#FFFFFF', false], node(['Lime', '#bfff00', false], empty, empty), node(['Yellow', '#FFFF00', false], empty, empty))
+            );
+
+        });
 
     it('search', function() {
         // assert.deepEqual(search('Yellow', 
